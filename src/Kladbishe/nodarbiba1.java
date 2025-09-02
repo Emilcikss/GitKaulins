6efe8ca2082a3817278e8c18d8c065093fcc7918
[Emils Glotovs] <[emilsglotovs@sk.lvt.lvgit config user.emailgit config --global user.email [emilsgltoovs@sk.lvt.lv]git config user.emailgit config --global use.email[emils.glotovs@sk.lvt.lv>
2025-09-02 12:02:11 +0300

    Izveidota metode <kaulinamesana>

--- src/Kladbishe/nodarbiba1.java
+++ src/Kladbishe/nodarbiba1.java
@@ -3,11 +3,13 @@ package Kladbishe;
 import java.util.Random;
 
 public class nodarbiba1 {
-
+static void mestkaulinu() {
+	int sk;
+	Random rand = new Random();
+	sk = rand.nextInt(6)+1;
+	System.out.println(sk + " - izkritusais skaitlis");
+}
 	public static void main(String[] args) {
-		int sk;
-		Random rand = new Random();
-		sk = rand.nextInt(6)+1;
-		System.out.println(sk);
+		mestkaulinu();
 	}
 }
