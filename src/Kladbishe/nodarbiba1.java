3593825978250392be1b1290ad8fc4a299616b4c
[Emils Glotovs] <[emilsglotovs@sk.lvt.lvgit config user.emailgit config --global user.email [emilsgltoovs@sk.lvt.lv]git config user.emailgit config --global use.email[emils.glotovs@sk.lvt.lv>
2025-09-02 11:42:20 +0300

    Skaitla uzmesana ir uztaisita

--- src/Kladbishe/nodarbiba1.java
+++ src/Kladbishe/nodarbiba1.java
@@ -1,5 +1,13 @@
 package Kladbishe;
 
+import java.util.Random;
+
 public class nodarbiba1 {
 
+	public static void main(String[] args) {
+		int sk;
+		Random rand = new Random();
+		sk = rand.nextInt(6)+1;
+		System.out.println(sk);
+	}
 }
